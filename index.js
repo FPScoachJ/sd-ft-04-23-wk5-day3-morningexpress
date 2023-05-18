3ce87c92a5b71a9ee5262d1130a0b5ac997ba6d5
Justin Anzalone <justin.m.anzalone@gmail.com>
2023-05-18 07:07:22 -0700

    homework problems solved

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3003;
 app.use(express.json());
 
-const harryPotterStudents = [
+let harryPotterStudents = [
   {
     name: "Harry Potter",
     house: "Gryffindor",
@@ -49,11 +49,47 @@ const harryPotterStudents = [
 ];
 
 app.post("/break_student_wand", (req, res) => {
-  // add your code here
+  // {bloodStatus: “Muggle-Born”}
+  // break all the wands of whoever is a muggle
+  // {wand: “Broken Wand”}
+  // Justin
+  // Find all students with matching blood status
+  const matchedStudents = [];
+  harryPotterStudents.forEach((student) => {
+    if (student.bloodStatus === req.body.bloodStatus) {
+      student.wand = "Broken Wand";
+      matchedStudents.push(student.name);
+    }
+  });
+
+  if (matchedStudents.length === 0) {
+    res.status(404).send("No students found with that blood status!");
+  } else {
+    res.send(`Students whose blood status has changed and wands have broken: ${matchedStudents.join(", ")}`);
+  }
+  console.log(harryPotterStudents);
 });
 
 app.post("/change_student_pet", (req, res) => {
   // add your code here
+  const indexOfStudentToUpdate = harryPotterStudents.findIndex(
+    (studs) => studs.name === req.body.name
+  );
+  if (indexOfStudentToUpdate === -1) {
+    res
+      .status(404)
+      .send("That student does not exist!");
+  } else {
+    if (req.body.name) {
+      harryPotterStudents[indexOfStudentToUpdate].pet = req.body.petToUpdateTo;
+      res.send(
+        `This is your newly updated student ${harryPotterStudents[indexOfStudentToUpdate].pet}`
+      );
+    } else {
+      res.send("You need to provide a name of a student and a pet to update.");
+    }
+    console.log(harryPotterStudents);
+  }
 });
 
 app.post("/change_student_house", (req, res) => {
@@ -62,6 +98,12 @@ app.post("/change_student_house", (req, res) => {
 
 app.post("/delete_student", (req, res) => {
   // add your code here
+  const filteredStudents = harryPotterStudents.filter(
+    (studs) => studs.name !== req.body.name
+  );
+  harryPotterStudents = filteredStudents;
+  res.send(harryPotterStudents);
+  console.log(harryPotterStudents);
 });
 
 app.post("/update_student", (req, res) => {
@@ -70,10 +112,15 @@ app.post("/update_student", (req, res) => {
 
 app.post("/create_student", (req, res) => {
   // add your code here
+  harryPotterStudents.push(req.body);
+  res.send(harryPotterStudents);
+  console.log(harryPotterStudents);
 });
 
 app.get("/get_students", (req, res) => {
   // add your code here
+  res.json(harryPotterStudents);
+  console.log(harryPotterStudents);
 });
 
 app.listen(PORT, () => console.log(`Listening in on port ${PORT}`));
